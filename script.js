a7b5f160006dce4c84c1f0f8dced4d496c96a17c
Manuel Gonzalez <87993163072@senacrs.edu.br>
2025-03-29 14:27:13 -0300

    v01

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("ligar").addEventListener("click", function(event) {
+    document.getElementById("lampada").src = "https://th.bing.com/th/id/OIP.fG_-YRBLFGHJhmMoHvFOowAAAA?rs=1&pid=ImgDetMain";
+});
+
+document.getElementById("desligar").addEventListener("click", function(event) {
+    document.getElementById("lampada").src = "https://www.eleyo.com/hubfs/Lightbulb.jpg";
+});
